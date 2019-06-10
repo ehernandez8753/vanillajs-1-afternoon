bfb32b6724917664fffee4753d13fe3a8f00548f
Esteban Hernandez <contact@mksmdesign.com>
2019-06-10 16:36:02 -0600

    Added features to poker project

--- Part2/index.js
+++ Part2/index.js
@@ -1,5 +1,6 @@
 let inputSetId = document.getElementById("idInput");
 let inputSetColor = document.getElementById("colorInput");
+let toRandomizeCards = false;
 
 console.log(inputSetColor, inputSetId);
 
@@ -10,6 +11,9 @@ function setCard(){
 
 function resetCards(){
     let card = document.getElementById("diamonds");
+    let mainBodyUI = document.getElementById("mainUI");
+
+    mainBodyUI.style.backgroundColor = "white";
     card.style.color = "gray";
     card = document.getElementById("clubs");
     card.style.color = "gray";
@@ -18,3 +22,28 @@ function resetCards(){
     card = document.getElementById("spades");
     card.style.color = "gray";
 }
+
+function changeRandomizeVal(){
+    if(toRandomizeCards === false) toRandomizeCards = true;
+    else toRandomizeCards = false;
+}
+
+window.setInterval(function randomizeCards(){
+    if(toRandomizeCards === true){
+        let mainBodyUI = document.getElementById("mainUI");
+        let card = document.getElementById("diamonds");
+
+        mainBodyUI.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1})`;
+        card = document.getElementById("diamonds");
+        card.style.color = `rgb(${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1})`;
+        card = document.getElementById("clubs");
+        card.style.color = `rgb(${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1})`;
+        card = document.getElementById("hearts");
+        card.style.color = `rgb(${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1})`;
+        card = document.getElementById("spades");
+        card.style.color = `rgb(${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1}, ${Math.floor(Math.random() * 255) + 1})`;
+    }
+
+
+}, 100);
+
